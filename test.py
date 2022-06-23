acdf90cfcf038cc67e6dbd5d0a038fa373fce88b
Simon Ott <simonott@gmx.at>
2022-06-23 15:53:41 +0200

    implemented qed source, strategy_qa full

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
-"""
-from datasets import load_dataset
-dsd = load_dataset("thoughtsource/datasets/open_book_qa/open_book_qa.py")
+import json
+from re import X
+from datasets import DownloadConfig, DownloadMode, load_dataset
+dsd = load_dataset("thoughtsource/datasets/strategy_qa/strategy_qa.py")
 
 print(dsd)
 print(dsd["train"][0])
-"""
 
+"""
 from thoughtsource import load_datasets
 
-a = load_datasets(["gsm8k", "open_book_qa"])
+a = load_datasets(["qed"])
 print(a)
+"""
