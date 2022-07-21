fb154ce3411218708e5d764ca0e2eb55a270335f
Simon Ott <simonott@gmx.at>
2022-07-21 17:29:12 +0200

    reworked dataloader

--- dataloader/dataloader/__init__.py
+++ dataloader/dataloader/__init__.py
@@ -1,3 +1,3 @@
 __version__ = "0.0.1"
 
-from .dataloader import load_datasets
+from .dataloader import Collection
--- dataloader/dataloader/dataloader.py
+++ dataloader/dataloader/dataloader.py
@@ -1,47 +1,71 @@
 import pathlib
 import os
+import pandas as pd
 from datasets import load_dataset
 
 class Collection:
 
-    def __init__(self):
+    def __init__(self, names=None):
         self._cache = {}
+        if names == "all":
+            self.load_datasets()
+        elif isinstance(names, list):
+            self.load_datasets(names)
 
     def __getitem__(self, key):
+        if key not in self._cache:
+            self.load_datasets(names=[key])
         return self._cache[key]
   
     def __setitem__(self, key, newvalue):
         self._cache[key] = newvalue
 
-    @property
-    def statistics(self):
-        table = "| Name | Train | Validation | Test |\n"
-        table += "|----|----|----|----|\n"
+    def __iter__(self):
+        yield from self._cache.items()
 
-        for name in self._cache.keys():
-            table = table + f"| {name} | {self._cache[name]['train'].num_rows} | {self._cache[name]['validation'].num_rows if 'validation' in self._cache[name] else '-'} | {self._cache[name]['test'].num_rows if 'test' in self._cache[name] else '-'} |" + "\n"
-        return table
-
-
-
-
-def load_datasets(names=None):
-    path_to_here = pathlib.Path(__file__).parent.absolute()
-    path_to_biodatasets = (path_to_here / "datasets").resolve()
-    if names is None:
-        dataloader_scripts = sorted(
-            path_to_biodatasets.glob(os.path.join("*", "*.py"))
-        )
-        dataloader_scripts = [
-            (el.name.replace(".py", ""), el) for el in dataloader_scripts if el.name != "__init__.py"
+    def __repr__(self):
+        data = [
+            (
+                name, 
+                self._cache[name]['train'].num_rows,
+                self._cache[name]['validation'].num_rows if 'validation' in self._cache[name] else '-',
+                self._cache[name]['test'].num_rows if 'test' in self._cache[name] else '-'
+            )
+            for name in self._cache.keys()
         ]
-    else:
-        dataloader_scripts = [
-            (name, path_to_biodatasets / name / (name + ".py")) for name in names
-        ]
-    
-    datasets = Collection()
-    for name, scripts in dataloader_scripts:
-        print(f"Loading {name}")
-        datasets[name] = load_dataset(str(scripts))
-    return datasets
+        table = pd.DataFrame.from_records(data, columns=["Name", "Train", "Valid", "Test"])
+        table = table.to_markdown(index=False, tablefmt="github")
+        not_loaded = [name for name, _ in self._find_datasets() if name not in self._cache]
+        return table + "\n\nNot loaded: " + str(not_loaded)
+
+    def _find_datasets(self, names=None):
+        path_to_biodatasets = (pathlib.Path(__file__).parent.absolute() / "datasets").resolve()
+        if names is None:
+            dataloader_scripts = sorted(
+                path_to_biodatasets.glob(os.path.join("*", "*.py"))
+            )
+            dataloader_scripts = [
+                (el.name.replace(".py", ""), el) for el in dataloader_scripts if el.name != "__init__.py"
+            ]
+        else:
+            dataloader_scripts = [
+                (name, path_to_biodatasets / name / (name + ".py")) for name in names
+            ]
+        return dataloader_scripts
+
+    def load_datasets(self, names=None):
+        """
+        It takes a list of names, finds the corresponding scripts, and loads the datasets
+        
+        :param names: A list of dataset names to load. If None, all datasets are loaded
+        """
+        datasets = self._find_datasets(names)
+        for name, script in datasets:
+            print(f"Loading {name}")
+            self._cache[name] = load_dataset(str(script))
+
+    def dump(self, path_to_directory = "./dump"):
+        for name, dataset_dict in self._cache.items():
+            for split, data in dataset_dict.items():
+                data.to_json(pathlib.Path(path_to_directory) / name / f"{split}.json")
+
