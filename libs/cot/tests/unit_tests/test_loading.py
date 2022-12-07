547070fac0bccde27ce03a2921ecae00fa3b73de
Simon Ott <simonott@gmx.at>
2022-12-07 11:44:40 +0100

    Add test for source

--- libs/cot/tests/unit_tests/test_loading.py
+++ libs/cot/tests/unit_tests/test_loading.py
@@ -39,9 +39,17 @@ def test_load_data_length():
         collection = Collection.from_json("worldtree_100_dataset.json")
         assert len(collection._cache["worldtree"]["train"]) == 100
 
-def test_unique_id() -> None:
+def test_thougthsource() -> None:
+    collection = Collection("all", recahce=True)
     """Test that id is unique within a dataset"""
-    collection = Collection("all")
+    for name, dataset in collection:
+        for split in dataset:
+            pd_ = dataset[split].to_pandas()
+            assert (len(pd_["id"]) == pd_["id"].nunique()), f"IDs are not unique in {name} {split}"
+
+def test_source() -> None:
+    collection = Collection("all", recahce=True, load_source=True)
+    """Test that id is unique within a dataset"""
     for name, dataset in collection:
         for split in dataset:
             pd_ = dataset[split].to_pandas()
