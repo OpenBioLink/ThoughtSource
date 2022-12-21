b900abdeb55453e7c0878d945ec35a8b114ccf50
Matthias Samwald <samwald@gmx.at>
2022-12-21 13:59:52 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/OpenBioLink/ThoughtSource""
    This reverts commit 1a1793d4c0e248470127c1078ab21ecc92be3811.

---
+++ libs/cot/tests/unit_tests/utils.py
@@ -0,0 +1,41 @@
+import os
+from contextlib import contextmanager
+from dataclasses import asdict
+from pathlib import Path
+
+from cot import Collection
+from cot.config import Config
+
+
+@contextmanager
+def chdir(path):
+    """Switch working directory to path and back to base directory"""
+    base_dir = Path().absolute()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(base_dir)
+
+
+def simple_config():
+    """Simple config for testing"""
+    config = {
+        "debug": True,
+        "multiple_choice_answer_format": "Letters",
+        "instruction_keys": ["qa-01"],
+        "cot_trigger_keys": ["kojima-01"],
+        "answer_extraction_keys": ["kojima-01"],
+        "warn": False,
+        "verbose": False,
+    }
+    config = Config(**config)
+    config = asdict(config)
+    return config
+
+
+def get_test_collection(name: str) -> Collection:
+    """Load a test collection from the data folder"""
+    with chdir("tests/unit_tests/data"):
+        collection = Collection.from_json(f"{name}.json")
+    return collection
