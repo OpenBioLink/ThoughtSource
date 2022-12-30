1e76dbdd376b8a45aec21c41e16cbd1c58d517f4
Konstantin Hebenstreit <konstantin.hebenstreit@posteo.at>
2022-12-30 10:39:56 +0100

    first save

--- libs/cot/cot/evaluate.py
+++ libs/cot/cot/evaluate.py
@@ -5,19 +5,52 @@ import datasets as ds
 
 
 # ver 0.2
-def clean(type_, pred):
+def clean(type_, pred, num_choices):
     if type_ == "multiplechoice":
         # TODO: BIG change necessary. Important to correct, add free text answers
         # Will make big mistakes, the code is I think from another paper
-        pred = re.findall(r"A|B|C|D", pred)
-        if len(pred):
-            pred = pred[0]
+        choices = [chr(65 + i) for i in range(num_choices)]
+
+        # multiple choice
+        "Therefore, among A through E, the answer is (C)"
+        "So the answer is (a)"
+        "Therefore, among A through E, the answer is A."
+
+        # boolean
+        "Therefore, the answer (Yes or No) is No."
+        "So the answer is yes."
+        "Therefore, the answer (Yes or No) is NO."
+        
+
+        possible_answers_sequences = [
+            r"So the answer is",
+            r"Therefore, the answer is",
+            r"The answer is",
+            r"Answer is",
+            r"Answer",
+            r"The correct answer is",
+            r"The correct answer",
+            r"Correct answer is",
+            r"Correct answer",
+            r"Among . through ., the answer is",
+            r"Among . through ., the correct answer is",]
+        
+        for seq in possible_answers_sequences:
+            pred = re.sub(seq, "", pred)
+
+        pred_found = re.findall(r'|'.join(choices), pred)
+        if len(pred_found):
+            # if multiple choices are found, take the last one
+            pred_selected = pred_found[-1]
+            # return warning if multiple choices are found
+            import warnings
+            warnings.warn(f"Multiple choices found in prediction: {pred_found}")
         else:
-            pred = ""
+            pred_selected = ""
     else:
         raise ValueError("type is not supported ...")
 
-    return pred
+    return pred_selected
 
 
 def is_correct(type_, pred, gold):
@@ -26,9 +59,10 @@ def is_correct(type_, pred, gold):
 
 
 def answer_to_multiplechoice(answer, choices):
+    num_choices = len(choices)
     for ix, choice in enumerate(choices):
         if choice == answer:
-            return chr(65 + ix)
+            return (num_choices, chr(65 + ix))
     raise ValueError("Thats weird, gold-answer not found in choices")
 
 
@@ -38,13 +72,13 @@ def evaluate_sample(example, type_):
     gold_answer = example["answer"][0]
 
     if type_ == "multiplechoice":
-        gold_answer = answer_to_multiplechoice(gold_answer, example["choices"])
+        num_choices, gold_answer = answer_to_multiplechoice(gold_answer, example["choices"])
 
     for cot in example["generated_cot"]:
         for answer in cot["answers"]:
             answer_str = answer["answer"]
-            answer_str = clean(type_, answer_str)
-            if is_correct(type_, answer_str, gold_answer):
+            answer_str_cleaned = clean(type_, answer_str, num_choices)
+            if is_correct(type_, answer_str_cleaned, gold_answer):
                 answer["correct_answer"] = True
             else:
                 answer["correct_answer"] = False
