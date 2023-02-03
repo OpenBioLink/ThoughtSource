45344804a42fad222995c3a6d3e4d3251a512681
Konstantin Hebenstreit <konstantin.hebenstreit@posteo.at>
2023-02-03 14:33:54 +0100

    integration tests loading specific generated cots

--- libs/cot/tests/integration_tests/test_load_collections.py
+++ libs/cot/tests/integration_tests/test_load_collections.py
@@ -18,3 +18,30 @@ def test_thoughtsource() -> None:
 def test_source() -> None:
     collection = Collection("all", generate_mode="recache", source=True)
     assert collection
+
+
+def test_keep_generated_cots() -> None:
+    # load collection with pregenerated cots of all authors
+    commonsense = Collection(["commonsense_qa"], verbose=False, load_pregenerated_cots="all")
+    commonsense_1 = commonsense.select(split="validation", number_samples=1)
+    commonsense_1_all = commonsense_1.to_json()
+    # check if both authors are loaded
+    assert len(commonsense_1["commonsense_qa"]["validation"][0]["generated_cot"]) == 2
+
+    commonsense = Collection(["commonsense_qa"], verbose=False, load_pregenerated_cots=["kojima","wei"])
+    commonsense_1 = commonsense.select(split="validation", number_samples=1)
+    commonsense_1_both = commonsense_1.to_json()
+    # check if both authors are kept
+    assert commonsense_1_all == commonsense_1_both
+
+    commonsense = Collection(["commonsense_qa"], verbose=False, load_pregenerated_cots=["kojima"])
+    commonsense_1_kojima = commonsense.select(split="validation", number_samples=1)
+    # check if only one author is loaded
+    assert len(commonsense_1_kojima["commonsense_qa"]["validation"][0]["generated_cot"]) == 1
+    assert commonsense_1_kojima["commonsense_qa"]["validation"][0]["generated_cot"][0]["author"] == "kojima"
+
+    commonsense = Collection(["commonsense_qa"], verbose=False, load_pregenerated_cots=["wei"])
+    commonsense_1_wei = commonsense.select(split="validation", number_samples=1)
+    # check if only one author is loaded
+    assert len(commonsense_1_wei["commonsense_qa"]["validation"][0]["generated_cot"]) == 1
+    assert commonsense_1_wei["commonsense_qa"]["validation"][0]["generated_cot"][0]["author"] == "wei"
