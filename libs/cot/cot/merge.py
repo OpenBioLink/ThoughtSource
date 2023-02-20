17395e57c5a6c6ca0445978281626baf66c10fc1
Konstantin Hebenstreit <konstantin.hebenstreit@posteo.at>
2023-02-20 18:57:11 +0100

    add merge all files in directory function

--- libs/cot/cot/merge.py
+++ libs/cot/cot/merge.py
@@ -50,3 +50,15 @@ def merge(base_collection, head_collection):
     merged_json = merger.merge(base_collection.to_json(), head_collection.to_json())
     # cannot call Collection.from_json because of circular dependency otherwise
     return base_collection.__class__.from_json(merged_json)
+
+# merge all files in a directory
+def merge_all_files_in_dir(dir):
+    from cot import Collection
+    filenames = os.listdir(dir)
+    filenames = [filename for filename in filenames if filename.endswith(".json")]
+    filenames = sorted(filenames)
+    collection = Collection.from_json(os.path.join(dir, filenames[0]))
+    for filename in filenames[1:]:
+        collection_add = Collection.from_json(os.path.join(dir, filename))
+        collection = collection.merge(collection_add)
+    return collection
