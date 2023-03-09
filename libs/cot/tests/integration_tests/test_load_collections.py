5d3d125ada5f6184e38119a3e6f1194967b51880
Konstantin Hebenstreit <konstantin.hebenstreit@posteo.at>
2023-03-09 14:32:50 +0100

    tests adapted for select_generated_cots

--- libs/cot/tests/integration_tests/test_load_collections.py
+++ libs/cot/tests/integration_tests/test_load_collections.py
@@ -20,26 +20,35 @@ def test_source() -> None:
 
 def test_keep_generated_cots() -> None:
     # load collection with pregenerated cots of all authors
-    commonsense = Collection(["commonsense_qa"], verbose=False, load_pregenerated_cots="all")
+    commonsense = Collection(["commonsense_qa"], verbose=False, load_pregenerated_cots=True)
     commonsense_1 = commonsense.select(split="validation", number_samples=1)
     commonsense_1_all = commonsense_1.to_json()
     # check if both authors are loaded
     assert len(commonsense_1["commonsense_qa"]["validation"][0]["generated_cot"]) == 2
 
-    commonsense = Collection(["commonsense_qa"], verbose=False, load_pregenerated_cots=["kojima", "wei"])
-    commonsense_1 = commonsense.select(split="validation", number_samples=1)
+    # select authors
+    commonsense.select_generated_cots(author=["kojima", "wei"])
     commonsense_1_both = commonsense_1.to_json()
     # check if both authors are kept
     assert commonsense_1_all == commonsense_1_both
 
-    commonsense = Collection(["commonsense_qa"], verbose=False, load_pregenerated_cots=["kojima"])
+    # select authors
     commonsense_1_kojima = commonsense.select(split="validation", number_samples=1)
+    commonsense_1_kojima.select_generated_cots(author="kojima")
     # check if only one author is loaded
     assert len(commonsense_1_kojima["commonsense_qa"]["validation"][0]["generated_cot"]) == 1
     assert commonsense_1_kojima["commonsense_qa"]["validation"][0]["generated_cot"][0]["author"] == "kojima"
 
-    commonsense = Collection(["commonsense_qa"], verbose=False, load_pregenerated_cots=["wei"])
+    # select authors
     commonsense_1_wei = commonsense.select(split="validation", number_samples=1)
+    commonsense_1_wei.select_generated_cots(author="wei")
     # check if only one author is loaded
     assert len(commonsense_1_wei["commonsense_qa"]["validation"][0]["generated_cot"]) == 1
     assert commonsense_1_wei["commonsense_qa"]["validation"][0]["generated_cot"][0]["author"] == "wei"
+
+    # select cot_trigger
+    commonsense_1_kojima = commonsense.select(split="validation", number_samples=1)
+    commonsense_1_kojima.select_generated_cots(cot_trigger="kojima-01")
+    # check if only one author is loaded
+    assert len(commonsense_1_kojima["commonsense_qa"]["validation"][0]["generated_cot"]) == 1
+    assert commonsense_1_kojima["commonsense_qa"]["validation"][0]["generated_cot"][0]["author"] == "kojima"
